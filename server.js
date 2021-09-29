45b732c290cdcb57532c30e07ed3cfe44f71c37d
ZahraZfr <Zahra.Z@m.lo.agency>
2021-09-29 21:25:21 +0330

    feat: add logger to project

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+
+require("dotenv").config();
+
 //Create a server that can send back static files
 const http = require("http");
 const url = require("url");
@@ -5,14 +8,22 @@ const fs = require("fs");
 
 //npm i mime-types
 const lookup = require("mime-types").lookup;
+// logger
+const {Logger} = require("@lo-agency/logger")
 
 const server = http.createServer((req, res) => {
   if (req.method === 'POST') {
     req.on("data", function (data) {
-      console.log(data.toString());
+
+      Logger.debug(`data.toString()`)
+
+      // console.log(data.toString());
       fs.appendFile("information.txt", data.toString(), function (err) {
         if (err) {
-          throw err; console.log("saved in file");
+
+          Logger.error(err)
+
+          return
         }
       })
     })
@@ -28,18 +39,20 @@ const server = http.createServer((req, res) => {
     if (path == "") {
       path = "index.html";
     }
-    console.log(`Requested path ${path} `);
+    // console.log(`Requested path ${path} `);
+    Logger.info(`requested path ${path}`)
 
     let file = __dirname + "/public/" + path;
     //async read file function uses callback
     fs.readFile(file, function (err, content) {
       if (err) {
-        console.log(`File Not Found ${file}`);
+        Logger.error(`file not found ${file}`)
         res.writeHead(404);
         res.end();
       } else {
         //specify the content type in the response
-        console.log(`Returning ${path}`);
+        // console.log(`Returning ${path}`);
+        Logger.info(`returning ${path}`)
         res.setHeader("X-Content-Type-Options", "nosniff");
         let mime = lookup(path);
         res.writeHead(200, { "Content-type": mime });
@@ -51,5 +64,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(7000, "localhost", () => {
-  console.log("Listening on port 7000");
+  // console.log("Listening on port 7000");
+  Logger.info(`listening on port 7000`)
 });
